e0fd6cc94e6bde657630df39b027196d4f6efd85
Alexis Comesana Silvera <prog.alexis.cs@protonmail.com>
2021-11-30 23:19:16 -0200

    Primeiro Programa em Kotlin

---
+++ app/src/main/java/com/example/listadetarefas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.listadetarefas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Olá Mundo!", Toast.LENGTH_LONG).show()
+    }
+}
